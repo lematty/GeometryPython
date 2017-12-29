da230a40a77002194bab86f592f9cbf0cec2f7e4
Chris LeMatty <clematty@gmail.com>
2017-12-29 16:07:36 +0100

    Cleanup

--- datamodel/Circle.py
+++ datamodel/Circle.py
@@ -11,15 +11,14 @@ class Circle(Shape):
 
     def __init__(self, radius):
         self.r = radius
-        super(Circle, self).__init__()
 
     # Calculate area
     def getArea(self):
         return pi * self.r ** 2
 
     # Calculate Perimeter
-#     def getPerimeter(self):
-#         return 2 * pi * self.r
+    def getPerimeter(self):
+        return 2 * pi * self.r
 
     def __repr__(self):
         return "radius = '{}'".format(self.r)
